5ca8562a8ab41fa36b886a034bdd415975ba5bbe
Subhankar Roy <subhankarroylive@gmail.com>
2026-01-10 16:33:03 +0530

    ENV Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   AES_KEY: '127131361926BYIBXIUBVSXIY',
   APP_NAME: 'Elexify Industries Control Panel',
-  APP_URL: 'https://admin.rudrakshavalley.com',
-  API_URL: 'https://api.rudrakshavalley.com/api/v1/',
+  APP_URL: 'https://inventory.elexify.com',
+  API_URL: 'https://api.elexify.online/api/v1/',
   X_API_KEY: 'Ip2A4a02I1r1I9dE1iSnA0S6aB1tE5WS',
   RUDRAKSHA: '68b47eca5ca7447af2874158',
 };
